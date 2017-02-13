86f1bd4aa02d4635bfaefa552b80eab58d3cc3c7
Morten Telling <mtelling1995@gmail.com>
2017-02-13 13:49:35 +0100

    Update my_chart.js

--- public/js/my_chart.js
+++ public/js/my_chart.js
@@ -32,7 +32,7 @@ var updateChart = function(labels, temps, humidities) {
                     borderColor: "rgba(91, 97, 120, 0.5)",
                     data : humidities,
                     yAxisID: "y-axis-humidity",
-                    lineTension: 0.0,
+                    lineTension: 0.2,
                     pointRadius: 0,
                     pointHoverRadius: 0
                 }
